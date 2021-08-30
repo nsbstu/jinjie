011bace294b3ef78eeb5b2b31d80a1ed3c691c2f
牛世博 <964851283@qq.com>
2021-08-30 20:48:03 +0800

    new jjj

---
+++ daynew/src/cn/itcast/demo01.java
@@ -0,0 +1,15 @@
+package cn.itcast;
+/**
+ * @author Yeek
+ * @version 5.0
+ */
+public class demo01 {
+
+        /**
+         * @param name 该参数指定向谁打招呼
+         * @return 返回打招呼的字符
+         */
+    public String hello (String name){
+        return  name + ",你好！";
+    }
+}
